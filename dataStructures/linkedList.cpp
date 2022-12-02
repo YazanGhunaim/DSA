78d50ac917aec55c5a22eb9e0e14a016cf80cbda
Yazan Ghunaim <yazanghunaim07@gmail.com>
2022-12-02 17:34:44 +0100

    working basic functions

--- dataStructures/linkedList.cpp
+++ dataStructures/linkedList.cpp
@@ -1,112 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 // node of a linked list
-typedef struct node {
+typedef struct node
+{
 	int data;
-	node *next;	
-}node;
+	node *next;
+} node;
 
 // returns number of elements in a linked list
-int size(node *head) {
+int size(node *head)
+{
+
 	int size = 0;
+	node *temp = head;
 
-	while(head != NULL){
+	while (temp != NULL)
+	{
 		size++;
-		head = head->next;
+		temp = temp->next;
 	}
-
-	printf("Linked List contains %d elements.\n", size);
 	return size;
 }
 
-// initializes three nodes in the list
-void init_list(node *p1, node *p2, node *p3){
-	p1->data = 4;
-	p1->next = p2;
-
-	p2->data = 2;
-	p2->next = p3;
-
-	p2->data = 0;
-	p2->next = NULL;
-}
-
 // function to check if list is empty
-bool empty(node *head) {
-	return head->next == NULL ? true : false;
+char *empty(node *head)
+{
+	char *yes = "yes";
+	char *no = "no";
+	return head == NULL ? yes : no;
 }
 
 // function to return the value at nth node
-int value_at(node *head, int n){
-	for(int i = 0; i < n; ++i)
+int value_at(node *head, int n)
+{
+	for (int i = 0; i < n; ++i)
 		head = head->next;
 
 	return head->data;
 }
 
-void print_list(node *head){
-	while(head != NULL){
-		printf("%d  ", head->data);
-		head = head->next;
+void print_list(node *head)
+{
+	node *temp = head;
+	while (temp != NULL)
+	{
+		printf("%d  ", temp->data);
+		temp = temp->next;
 	}
 	printf("\n");
 }
 
 // query for checking nth value
-int queryInput(node *head){
+void queryInput(node *head)
+{
 	int index;
-	printf("which index values would you like to check?\n");
-
-	while (scanf("%d", &index) == 1){
-	if (index >= (size(head)))
+	printf("which index values would you like to check? ");
+	while (scanf("%d", &index) == 1)
 	{
-		printf("\nnode is out of bounds... try again:\n");
-		continue;
+		if (index >= (size(head)))
+		{
+			printf("\nnode is out of bounds... try again: ");
+			continue;
+		}
+		printf("value is %d.\n", value_at(head, index));
 	}
-	printf("value is %d.\n", value_at(head,index));
-	}	
-
-	return index;
 }
 
 // add a node at the beginning of the list
-node * push_front(int data, node *head){
+void push_front(int data, node **head)
+{
 	if (head == NULL)
 	{
-		node * newNode = (node *)malloc(sizeof(*newNode));
+		node *newNode = (node *)malloc(sizeof(*newNode));
 		newNode->data = data;
-		head = newNode;
-
-		return newNode;
+		*head = newNode;
 	}
 	else
 	{
-		node * newNode = (node *)malloc(sizeof(*newNode));
+		node *newNode = (node *)malloc(sizeof(*newNode));
 		newNode->data = data;
-		newNode->next = head;
-		head = newNode;
-
-		return newNode;
+		newNode->next = *head;
+		*head = newNode;
 	}
 }
 
 int main(int argc, char const *argv[])
-{	
-	node p1,p2,p3;
+{
+	node *head = NULL;
 
-	//initializing
-	init_list(&p1,&p2,&p3);
+	push_front(6, &head);
+	push_front(4, &head);
+	push_front(2, &head);
+	push_front(0, &head);
+	push_front(9, &head);
 
-	printf("is list empty? %d.\n", empty(&p1));
-	size(&p1);
+	printf("is list empty? %s.\n", empty(head));
+	printf("Linked List contains %d elements.\n", size(head));
 
-	queryInput(&p1);
+	// check value of nodes at nth index
+	queryInput(head);
 
-	// push_front(6,&p1);
-	// print_list(&p1);
-	
+	print_list(head);
 
 	return 0;
 }
